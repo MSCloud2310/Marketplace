84a803567b1232c5a25b82d5cd9dd0a0c4444c3a
jsebastianherrera <juansehgu@gmail.com>
2023-03-26 21:21:10 -0500

    eureka

--- userManagment/src/main/java/com/javeriana/userManagment/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javeriana.userManagment.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.javeriana.userManagment.model.Client;
-
-public interface ClientRepository extends CrudRepository<Client, Long>{
-    
-}
--- userManagment/src/main/java/com/javeriana/userManagment/repository/SocialNetworkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javeriana.userManagment.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.javeriana.userManagment.model.SocialNetwork;
-
-public interface SocialNetworkRepository extends CrudRepository<SocialNetwork ,Long>{
-    
-}
