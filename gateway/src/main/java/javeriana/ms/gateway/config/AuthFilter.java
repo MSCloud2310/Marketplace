f9c7406aa325c7c7e786f6cc95273a91337b791e
pangolin <juansehgu@gmail.com>
2023-05-28 19:39:07 -0500

    Updated

--- gateway/src/main/java/javeriana/ms/gateway/config/AuthFilter.java
+++ gateway/src/main/java/javeriana/ms/gateway/config/AuthFilter.java
@@ -1,5 +1,6 @@
 package javeriana.ms.gateway.config;
 
+import lombok.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -10,39 +11,40 @@ import javeriana.ms.gateway.dto.UserDto;
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 
-    private final WebClient.Builder wBuilder;
+    private final WebClient.Builder webClientBuilder;
 
-    public AuthFilter(WebClient.Builder wBuilder){
-        this.wBuilder = wBuilder;
+    public AuthFilter(WebClient.Builder webClientBuilder) {
+        super(Config.class);
+        this.webClientBuilder = webClientBuilder;
     }
-    
-    public static class Config{
-        
+
+    public static class Config {
+
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                 throw new RuntimeException("Missin auth header");
             }
 
             String authToken = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
             String[] parts = authToken.split(" ");
 
-            if(parts.length != 2 || !"Bearer".equals(parts[0])){
+            if (parts.length != 2 || !"Bearer".equals(parts[0])) {
                 throw new RuntimeException("Incorrect structure");
             }
 
-            return wBuilder.build()
-                    .post()
-                    .uri("Ruta" + parts[1])
+            return webClientBuilder.build()
+                    .get()
+                    .uri(System.getenv("USERS_URL") + "/auth/validateToken?token=" + parts[1])
                     .retrieve().bodyToMono(UserDto.class)
                     .map(userDto -> {
                         exchange.getRequest()
-                                    .mutate()
-                                    .header("");
-                        
+                                .mutate()
+                                .header("auth-user", userDto.getToken());
+
                         return exchange;
                     }).flatMap(chain::filter);
         };
